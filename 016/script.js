2ce93f1eaea139adad371b97442c8e4b16ccd080
milenards <contatomillenarodrigues20@gmail.com>
2022-02-10 22:17:44 -0300

    Ativ. 16 Positivo ou negativo
    nessa atividade digitamos um número para descobrir se ele é positivo ou negativo.

---
+++ 016/script.js
@@ -0,0 +1,13 @@
+// programa que mostra na tela se o número é positivo ou negativo
+
+let valor
+
+console.log("Digite um número para verificarmos se é positivo ou negativo")
+valor = -5
+
+if(valor >=0){
+    console.log("Esse numero é: Positivo")
+}
+else{
+    console.log("esse número é: Negativo")
+}
