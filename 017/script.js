786ac74b86c106d878a71bc9e1308a1cfd92de9f
milenards <contatomillenarodrigues20@gmail.com>
2022-02-24 19:48:08 -0300

    exercicio 17

---
+++ 017/script.js
@@ -0,0 +1,14 @@
+//Faça um Programa que peça dois números e imprima o maior deles.
+let n1,n2, n3, maior
+n1=10
+n2=10
+n3=10
+maior = n1
+
+    if(n2 > n1 )
+    maior=n2
+
+    if(n3 > maior)
+    maior=n3      
+
+console.log ("O maior número é:", maior)
