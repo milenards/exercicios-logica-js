6291fdd316088db561818d5adf3d26d16cd948ab
milenards <contatomillenarodrigues20@gmail.com>
2022-03-03 20:15:34 -0300

    exercício 9 e 10
    graus

---
+++ 009/script.js
@@ -0,0 +1,8 @@
+
+let f, c
+
+console.log("Qual a temperatura em F°?")
+c= (150 - 32) * 5/9 
+//(150 é o graus em F)
+
+console.log("temperatura em C° é:",c)
---
+++ 010/script.js
@@ -0,0 +1,8 @@
+
+let f, c
+
+console.log("Qual a temperatura em C°?")
+
+f = (35 * 9/5) + 32
+    
+console.log("temperatura em F° é:",f)
