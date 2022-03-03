7a36741e5c2b5a02e2aa7ed04c1b754cf44c0fa3
milenards <contatomillenarodrigues20@gmail.com>
2022-03-03 20:15:11 -0300

    criação da interface do exercício1

--- 001/script.js
+++ 001/script.js
@@ -1,9 +1,18 @@
 //programa: Alo mundo
 //INICIO
     //ESCREVA "Alo mundo"
-    function alo(){
-    console.log("Alo Mundo")
+function alo(){
+    /*
+    saida de dados no corpo do navegador:
+    document.write("alo mundo")
+
+    em uma div, span, h1, etc:
+    document.querySelector("#resultado").innerHTML="alo mundo"
+
+    em um campo com texto:
+    document.querySelector("#resultado").value = "Alo mundo"
+    */
+    document.querySelector("#resultado").value = "Alo mundo"
 }
 
-alo()
 //FIM
